bec2db7306a3a943955f8c8d6aff2f01fe982cf7
David Ward <dward@wevad.com>
2007-08-22 19:51:52 +0000

    - CURRIKI-772  - Also change MimeType, Licence, and Framework custom classes
    svn@4494

--- database/src/resources/DB-Patch-CURRIKI-772.sql
+++ database/src/resources/DB-Patch-CURRIKI-772.sql
@@ -8,3 +8,6 @@
 --- **** Update org.gelc to org.curriki
 
 UPDATE xwikidoc SET XWD_CUSTOM_CLASS = 'org.curriki.xwiki.plugin.asset.Asset' WHERE XWD_CUSTOM_CLASS = 'org.gelc.xwiki.plugins.assets.Asset';
+update xwikidoc set XWD_CUSTOM_CLASS = 'org.curriki.xwiki.plugin.mimetype.MimeType' where XWD_CUSTOM_CLASS = 'org.gelc.xwiki.plugins.mime.MimeType';
+update xwikidoc set XWD_CUSTOM_CLASS = 'org.curriki.xwiki.plugin.licence.Licence' where XWD_CUSTOM_CLASS = 'org.gelc.xwiki.plugins.licence.Licence';
+update xwikidoc set XWD_CUSTOM_CLASS = 'org.curriki.xwiki.plugin.framework.Framework' where XWD_CUSTOM_CLASS = 'org.gelc.xwiki.plugins.framework.Framework';
