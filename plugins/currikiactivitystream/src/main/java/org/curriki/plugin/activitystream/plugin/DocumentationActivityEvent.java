d445a896facf5dfd79a2e25c69c165a14940830e
Marius Dumitru Florea <mariusdumitru.florea@xwiki.com>
2008-02-18 08:02:48 +0000

    CURRIKI-1636
    svn@7787

--- plugins/currikiactivitystream/src/main/java/org/curriki/plugin/activitystream/plugin/DocumentationActivityEvent.java
+++ plugins/currikiactivitystream/src/main/java/org/curriki/plugin/activitystream/plugin/DocumentationActivityEvent.java
@@ -53,7 +53,7 @@ public class DocumentationActivityEvent extends ActivityEvent
             if (tag != null) {
                 docTitle = doc.getTitle();
                 docType = tag.getStringValue("tags");
-                docLink = "[" + docTitle + ">" + doc.getExternalURL("view", context) + "]";
+                docLink = "[" + docTitle + ">" + doc.getFullName().replaceAll("@", "%40") + "]";
             }
         } catch (XWikiException e) {
         }
