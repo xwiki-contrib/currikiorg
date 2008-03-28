cd50cf074aa3926a16c68112247001466b156ec0
David Ward <dward@wevad.com>
2008-03-28 16:32:18 +0000

    - CURRIKI-1734  - Define variable for viditalk's script as we can't fix their script directly
    svn@8759

--- web/src/main/webapp/skins/curriki8/viditalk_stub.js
+++ web/src/main/webapp/skins/curriki8/viditalk_stub.js
@@ -8,3 +8,7 @@ function embedVidiPlayback(div_id, sc, mediaId, autoplay, width, height, stretch
 function embedVidiCapture(div_id, sc, captureType, autoPreview, allowImages, maxFilesizeMB, bypassPreview, allowPlaylists, bgFillColor, bgBorderColor, recLimit, horizontalPlaylist, allowActiveX){
 	viditalk_down(div_id);
 }
+
+var global_components_domain="components.viditalk.com";
+var CONST_COMPONENTS_FLASH_FOLDER="flash";
+var IS_VIDICOMPONENTS_INITIATED=false;
