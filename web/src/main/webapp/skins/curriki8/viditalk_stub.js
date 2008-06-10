1ce6a9092ca027761673233ab7bb02872dc9e14a
David Ward <dward@wevad.com>
2008-06-10 21:45:31 +0000

    - CURRIKI-2117  - viditalk_stub.js can now be loaded multiple times without bad effects
    svn@10238

--- web/src/main/webapp/skins/curriki8/viditalk_stub.js
+++ web/src/main/webapp/skins/curriki8/viditalk_stub.js
@@ -1,14 +1,19 @@
-// Stub functions for when VIDITalk cannot be reached.
-function viditalk_down(div_id){
-	document.getElementById(div_id).innerHTML="VIDITalk's server is currently down. Please try again later.";
-}
-function embedVidiPlayback(div_id, sc, mediaId, autoplay, width, height, stretchPlayer, skinLoc, shareUrl, allowFullScreen){
-	viditalk_down(div_id);
-}
-function embedVidiCapture(div_id, sc, captureType, autoPreview, allowImages, maxFilesizeMB, bypassPreview, allowPlaylists, bgFillColor, bgBorderColor, recLimit, horizontalPlaylist, allowActiveX){
-	viditalk_down(div_id);
-}
+var global_components_domain,
+    CONST_COMPONENTS_FLASH_FOLDER,
+    IS_VIDICOMPONENTS_INITIATED;
+if (!GLOBAL_FLAG_LOADED_VIDI_EMBED){
+	// Stub functions for when VIDITalk cannot be reached.
+	function viditalk_down(div_id){
+		document.getElementById(div_id).innerHTML="VIDITalk's server is currently down. Please try again later.";
+	}
+	function embedVidiPlayback(div_id, sc, mediaId, autoplay, width, height, stretchPlayer, skinLoc, shareUrl, allowFullScreen){
+		viditalk_down(div_id);
+	}
+	function embedVidiCapture(div_id, sc, captureType, autoPreview, allowImages, maxFilesizeMB, bypassPreview, allowPlaylists, bgFillColor, bgBorderColor, recLimit, horizontalPlaylist, allowActiveX){
+		viditalk_down(div_id);
+	}
 
-var global_components_domain="components.viditalk.com";
-var CONST_COMPONENTS_FLASH_FOLDER="flash";
-var IS_VIDICOMPONENTS_INITIATED=false;
+	global_components_domain="components.viditalk.com";
+	CONST_COMPONENTS_FLASH_FOLDER="flash";
+	IS_VIDICOMPONENTS_INITIATED=false;
+}
