803437b9376f80cc74f581d5b30328b39cf809c4
David Ward <dward@wevad.com>
2008-09-22 22:50:22 +0000

    - CURRIKI-2724  - Override ExtJS paging toolbar to remove refresh button
    svn@12926

--- web/src/main/javascript/ext/ExtJsOverride.js
+++ web/src/main/javascript/ext/ExtJsOverride.js
@@ -59,6 +59,68 @@ Ext.override(Ext.form.TriggerField, {
     }
 });
 
+// CURRIKI-2724
+// Remove "Refresh" button from paging toolbar
+Ext.override(Ext.PagingToolbar, {
+	// private
+    onRender : function(ct, position){
+        Ext.PagingToolbar.superclass.onRender.call(this, ct, position);
+        this.first = this.addButton({
+            tooltip: this.firstText,
+            iconCls: "x-tbar-page-first",
+            disabled: true,
+            handler: this.onClick.createDelegate(this, ["first"])
+        });
+        this.prev = this.addButton({
+            tooltip: this.prevText,
+            iconCls: "x-tbar-page-prev",
+            disabled: true,
+            handler: this.onClick.createDelegate(this, ["prev"])
+        });
+        this.addSeparator();
+        this.add(this.beforePageText);
+        this.field = Ext.get(this.addDom({
+           tag: "input",
+           type: "text",
+           size: "3",
+           value: "1",
+           cls: "x-tbar-page-number"
+        }).el);
+        this.field.on("keydown", this.onPagingKeydown, this);
+        this.field.on("focus", function(){this.dom.select();});
+        this.afterTextEl = this.addText(String.format(this.afterPageText, 1));
+        this.field.setHeight(18);
+        this.addSeparator();
+        this.next = this.addButton({
+            tooltip: this.nextText,
+            iconCls: "x-tbar-page-next",
+            disabled: true,
+            handler: this.onClick.createDelegate(this, ["next"])
+        });
+        this.last = this.addButton({
+            tooltip: this.lastText,
+            iconCls: "x-tbar-page-last",
+            disabled: true,
+            handler: this.onClick.createDelegate(this, ["last"])
+        });
+/* Removed for CURRIKI-2724
+        this.addSeparator();
+        this.loading = this.addButton({
+            tooltip: this.refreshText,
+            iconCls: "x-tbar-loading",
+            handler: this.onClick.createDelegate(this, ["refresh"])
+        });
+*/
+
+        if(this.displayInfo){
+            this.displayEl = Ext.fly(this.el.dom).createChild({cls:'x-paging-info'});
+        }
+        if(this.dsLoaded){
+            this.onLoad.apply(this, this.dsLoaded);
+        }
+    }
+});
+
 /*
 Ext.override(Ext.Shadow.prototype, {
 	realign: function(l, t, w, h){
