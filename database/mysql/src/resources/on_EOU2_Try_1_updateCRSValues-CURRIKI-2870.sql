e70008888a7237981962ec01eea7734f4b98bcc7
Alberto Buffagni <albertobuffagni@gmail.com>
2008-10-07 20:40:42 +0000

    CURRIKI-2870 new sql scripts
    svn@13389

---
+++ database/mysql/src/resources/on_EOU2_Try_1_updateCRSValues-CURRIKI-2870.sql
@@ -0,0 +1,61 @@
+﻿SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 200
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=10 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 800
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=20 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 600
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=40 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 400
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=60 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 100
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=80 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 700
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=30 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 500
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=50 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 300
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=70 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+
+commit;
---
+++ database/mysql/src/resources/on_PRODUCTION_updateCRSValues-CURRIKI-2870.sql
@@ -0,0 +1,39 @@
+﻿SET AUTOCOMMIT=0;
+START TRANSACTION;
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 200
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`='P' AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 800
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=3 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 600
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=2 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 400
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=1 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+UPDATE xwikistrings x
+SET x.`XWS_VALUE`= 100
+WHERE x.`XWS_NAME`='status' AND x.`XWS_VALUE`=0 AND x.`XWS_ID` IN
+(SELECT xo.`XWO_ID`
+FROM xwikiobjects xo
+WHERE xo.`XWO_CLASSNAME` = 'CRS.CurrikiReviewStatusClass');
+
+commit;
