93043857c69484ed3a05580bda240d11ad06cc59
Alberto Buffagni <albertobuffagni@gmail.com>
2008-10-22 21:44:32 +0000

    CURRIKI-3043 when setting P for Partner is complete, is returned to resource in View tab
    svn@13795

--- web/src/main/javascript/curriki/crs/partner-base.js
+++ web/src/main/javascript/curriki/crs/partner-base.js
@@ -1,5 +1,5 @@
 	Ext.ns("Curriki.module.partner");
-	Curriki.module.partner.confirm =	function(){
+	Curriki.module.partner.confirm =	function(redirectURL){
 		Ext.Msg.show({
    			title: _('curriki.crs.review.partnerconfirmtitle'),
    			msg: _('curriki.crs.review.partnerconfirmmessage'),
@@ -8,7 +8,7 @@
 				if (btn == "ok") {
 					var page = Ext.get('assetFullName').dom.value;
 					Curriki.assets.PartnerAsset(page, function(D){
-						window.location.pathname = 'xwiki/bin/view/CRS/Reviews';
+						window.location.pathname = redirectURL;
 					});
 				}
 				else{
