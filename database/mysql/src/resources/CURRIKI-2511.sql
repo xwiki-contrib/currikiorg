270cc3ddf6284d99fe030c0a5685253eb9343d45
David Ward <dward@wevad.com>
2008-10-22 15:57:41 +0000

    - CURRIKI-2870  - CURRIKI-2511 patch no longer needed
    svn@13787

--- database/mysql/src/resources/CURRIKI-2511.sql
+++
@@ -1,4 +0,0 @@
--- DEPRECATED - Use patch from CURRIKI-2870
---              on_PRODUCTION_updateCRSValues-CURRIKI-2870.sql
-
-SELECT 'Removed - Use patch from CURRIKI-2870 instead' message FROM DUAL;
