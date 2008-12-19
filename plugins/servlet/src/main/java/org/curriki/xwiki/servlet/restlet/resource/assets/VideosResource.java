5af2e9f55c6c2a4de5c6ba16e2f45436069754dd
Ludovic Dubost <ludovic@xwiki.com>
2008-12-19 16:46:18 +0000

    CURRIKI-3199 Data model and modular asset types and viewers
    svn@14890

--- plugins/servlet/src/main/java/org/curriki/xwiki/servlet/restlet/resource/assets/VideosResource.java
+++ plugins/servlet/src/main/java/org/curriki/xwiki/servlet/restlet/resource/assets/VideosResource.java
@@ -51,12 +51,19 @@ public class VideosResource extends BaseResource {
             throw error(Status.CLIENT_ERROR_NOT_FOUND, "No links found for "+assetName);
         }
 
+        String partner = null;
+        try {
+            partner = asset.getVideoPartner();
+        } catch (XWikiException e) {
+            partner = "";
+        }
+
         JSONArray jsonArray = new JSONArray();
 
         JSONObject json = new JSONObject();
         json.put("href", getChildReference(getRequest().getResourceRef(), "0"));
-        json.put("videoId", link);
-        json.put("partner", link);
+        json.put("video_id", link);
+        json.put("partner", partner);
 
         jsonArray.add(json);
 
@@ -72,12 +79,17 @@ public class VideosResource extends BaseResource {
         JSONObject json = representationToJSONObject(representation);
 
         String videoId;
-        String partner;
         try {
             videoId = json.getString("videoId");
-            partner = json.getString("partner");
         } catch (JSONException e) {
             throw error(Status.CLIENT_ERROR_NOT_ACCEPTABLE, "You must provide a video id.");
+        }
+
+        String partner;
+        try {
+            partner = json.getString("partner");
+        } catch (JSONException e) {
+            partner = "viditalk";
         }
 
         Asset asset = null;
