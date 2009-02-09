056fd836b454f96238fc519f7b3d83d10893daa3
David Ward <dward@wevad.com>
2009-02-09 21:21:37 +0000

    - CURRIKI-3716  - Change treeNodeUI so that the tabIndex is not 1 for the TOC
    svn@16501

--- web/src/main/webapp/js/curriki-module-toc-debug.js
+++ web/src/main/webapp/js/curriki-module-toc-debug.js
@@ -28,6 +28,7 @@ Toc.init = function(){
 		}
 	};
 
+	Ext.ns('Curriki.ui.treeLoader');
 	Curriki.ui.treeLoader.TOC = function(config){
 		Curriki.ui.treeLoader.TOC.superclass.constructor.call(this);
 	};
@@ -38,6 +39,48 @@ Toc.init = function(){
 		,setUniqueId:true
 		,hideInvalid:true
 	});
+
+	// private -- overridden to change tabIndex from the default "1"
+	Ext.tree.TreeNodeUI.prototype.renderElements = function(n, a, targetNode, bulkRender){
+		// add some indent caching, this helps performance when rendering a large tree
+		this.indentMarkup = n.parentNode ? n.parentNode.ui.getChildIndent() : '';
+
+		var cb = typeof a.checked == 'boolean';
+
+		var href = a.href ? a.href : Ext.isGecko ? "" : "#";
+		var buf = ['<li class="x-tree-node"><div ext:tree-node-id="',n.id,'" class="x-tree-node-el x-tree-node-leaf x-unselectable ', a.cls,'" unselectable="on">',
+			'<span class="x-tree-node-indent">',this.indentMarkup,"</span>",
+			'<img src="', this.emptyIcon, '" class="x-tree-ec-icon x-tree-elbow" />',
+			'<img src="', a.icon || this.emptyIcon, '" class="x-tree-node-icon',(a.icon ? " x-tree-node-inline-icon" : ""),(a.iconCls ? " "+a.iconCls : ""),'" unselectable="on" />',
+			cb ? ('<input class="x-tree-node-cb" type="checkbox" ' + (a.checked ? 'checked="checked" />' : '/>')) : '',
+			'<a hidefocus="on" class="x-tree-node-anchor" href="',href,'" tabIndex="20" ',
+			 a.hrefTarget ? ' target="'+a.hrefTarget+'"' : "", '><span unselectable="on">',n.text,"</span></a></div>",
+			'<ul class="x-tree-node-ct" style="display:none;"></ul>',
+			"</li>"].join('');
+
+		var nel;
+		if(bulkRender !== true && n.nextSibling && (nel = n.nextSibling.ui.getEl())){
+			this.wrap = Ext.DomHelper.insertHtml("beforeBegin", nel, buf);
+		}else{
+			this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
+		}
+		
+		this.elNode = this.wrap.childNodes[0];
+		this.ctNode = this.wrap.childNodes[1];
+		var cs = this.elNode.childNodes;
+		this.indentNode = cs[0];
+		this.ecNode = cs[1];
+		this.iconNode = cs[2];
+		var index = 3;
+		if(cb){
+			this.checkbox = cs[3];
+			// fix for IE6
+			this.checkbox.defaultChecked = this.checkbox.checked;			
+			index++;
+		}
+		this.anchor = cs[index];
+		this.textNode = cs[index].firstChild;
+	};
 
 	Toc.displayMainPanel = function(root){
 		// id = resource-toc
