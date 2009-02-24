a12725ea98e9f742047084a3a44cc8cd02b4b5bc
David Ward <dward@wevad.com>
2009-02-24 15:46:47 +0000

    - CURRIKI-3917  - Add another \ before x22 so that it will get interpreted instead of displaying x22
    svn@17020

--- plugins/asset/src/main/java/org/curriki/xwiki/plugin/asset/Util.java
+++ plugins/asset/src/main/java/org/curriki/xwiki/plugin/asset/Util.java
@@ -1,11 +1,9 @@
 package org.curriki.xwiki.plugin.asset;
 
 /**
- * Created by IntelliJ IDEA.
  * User: ludovic
  * Date: 10 févr. 2009
  * Time: 19:39:18
- * To change this template use File | Settings | File Templates.
  */
 public class Util {
     /**
@@ -16,7 +14,7 @@ public class Util {
     public static String escapeForJS(String origtext) {
         String text = origtext.replaceAll("\\\\", "\\\\");
         text = text.replaceAll("'", "\\\\'");
-        text = text.replaceAll("\"", "\\x22");
+        text = text.replaceAll("\"", "\\\\x22");
         return text;
     }    
 }
