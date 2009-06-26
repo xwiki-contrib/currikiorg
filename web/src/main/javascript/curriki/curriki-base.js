5cd163243fa9d78e6868b3fc4c5c1b33378e82b6
David Ward <dward@wevad.com>
2009-06-26 14:44:01 +0000

    - CURRIKI-4546  - Remove ; from Content-Type header sent
    svn@21628

--- web/src/main/javascript/curriki/curriki-base.js
+++ web/src/main/javascript/curriki/curriki-base.js
@@ -6,7 +6,7 @@ Ext.BLANK_IMAGE_URL = '/xwiki/skins/curriki8/extjs/resources/images/default/s.gi
 
 Ext.Ajax.defaultHeaders = {
 	 'Accept': 'application/json'
-	,'Content-Type': 'application/json; charset=utf-8;'
+	,'Content-Type': 'application/json; charset=utf-8'
 };
 Ext.Ajax.disableCaching=false;
 Ext.Ajax.timeout=120000;
