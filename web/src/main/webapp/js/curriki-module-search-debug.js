178073e17927a2854294cc744a30e606e23c25cd
Paul Librecht <paul@hoplahup.net>
2011-03-04 21:58:50 +0000

    use older search when having the debug flag. paul
    svn@35158

--- web/src/main/webapp/js/curriki-module-search-debug.js
+++ web/src/main/webapp/js/curriki-module-search-debug.js
@@ -580,7 +580,7 @@ data.init = function(){
 	data.store.results = new Ext.data.Store({
 		storeId: 'search-store-'+modName
 		,proxy: new Ext.data.HttpProxy({
-			url: '/currikiExtjs'
+			url: '/xwiki/bin/view/Search/Resources'//'/currikiExtjs'
 			,method:'GET'
 		})
 		,baseParams: { xpage: "plain", '_dc':(new Date().getTime()) }
