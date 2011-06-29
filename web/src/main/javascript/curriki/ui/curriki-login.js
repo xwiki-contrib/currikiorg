83b025f8f5720160ccd5179eb00448e72dd6ce44
Paul Librecht <paul@hoplahup.net>
2011-06-29 22:47:45 +0000

    getting in the look. paul
    svn@36406

--- web/src/main/javascript/curriki/ui/curriki-login.js
+++ web/src/main/javascript/curriki/ui/curriki-login.js
@@ -1,14 +1,15 @@
 
 Ext.ns('Curriki.ui.login');
 Curriki.ui.login.displayIntroStep = function() {
-    var w = 500, h=250;
+    var w = 700, h=300;
     Curriki.ui.login.loginDialog = new Ext.Window({
                 width:w,
                 height:h,
                 modal:true,
                 closable:true,
+                monitorResize: true,
                 title:"Login or Register",
-                html: "<iframe src='/xwiki/bin/loginsubmit/XWiki/XWikiLogin' width='"+w+"' height='"+h+"'/>"
+                html: "<iframe id='loginIframe' src='/xwiki/bin/loginsubmit/XWiki/XWikiLogin' width='"+w+"' height='"+h+"'/>"
             });
     Curriki.ui.login.loginDialog.show();
 };
