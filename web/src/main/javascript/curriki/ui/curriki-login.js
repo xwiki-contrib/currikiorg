18820ccb8324970132400ac34fe4f61b331a0356
Paul Librecht <paul@hoplahup.net>
2011-07-01 15:25:55 +0000

    Getting nice with dialogs. paul
    svn@36408

--- web/src/main/javascript/curriki/ui/curriki-login.js
+++ web/src/main/javascript/curriki/ui/curriki-login.js
@@ -1,7 +1,8 @@
 
 Ext.ns('Curriki.ui.login');
-Curriki.ui.login.displayIntroStep = function() {
+Curriki.ui.login.displayIntroStep = function(url) {
     var w = 700, h=300;
+    if(url.indexOf('?')>=0) url = url+"&framed=true"; else url=url+"?framed=true";
     Curriki.ui.login.loginDialog = new Ext.Window({
                 width:w,
                 height:h,
@@ -9,8 +10,7 @@ Curriki.ui.login.displayIntroStep = function() {
                 closable:true,
                 monitorResize: true,
                 title:"Login or Register",
-                html: "<iframe id='loginIframe' src='/xwiki/bin/loginsubmit/XWiki/XWikiLogin?framed=true' width='"+w+"' height='"+h+"'/>"
+                html: "<iframe id='loginIframe' src='"+url+"' width='"+w+"' height='"+h+"'/>"
             });
     Curriki.ui.login.loginDialog.show();
 };
-
--- web/src/main/webapp/js/curriki-main-debug.js
+++ web/src/main/webapp/js/curriki-main-debug.js
@@ -3342,8 +3342,9 @@ Ext.extend(Curriki.ui.treeLoader.Base, Ext.tree.TreeLoader, {
 });
 
 Ext.ns('Curriki.ui.login');
-Curriki.ui.login.displayIntroStep = function() {
+Curriki.ui.login.displayIntroStep = function(url) {
     var w = 700, h=300;
+    if(url.indexOf('?')>=0) url = url+"&framed=true"; else url=url+"?framed=true";
     Curriki.ui.login.loginDialog = new Ext.Window({
                 width:w,
                 height:h,
@@ -3351,7 +3352,7 @@ Curriki.ui.login.displayIntroStep = function() {
                 closable:true,
                 monitorResize: true,
                 title:"Login or Register",
-                html: "<iframe id='loginIframe' src='/xwiki/bin/loginsubmit/XWiki/XWikiLogin?framed=true' width='"+w+"' height='"+h+"'/>"
+                html: "<iframe id='loginIframe' src='"+url+"' width='"+w+"' height='"+h+"'/>"
             });
     Curriki.ui.login.loginDialog.show();
 };
