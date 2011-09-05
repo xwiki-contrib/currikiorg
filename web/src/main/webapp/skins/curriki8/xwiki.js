33e34184c69a4f95bcca340c4beea41429e80bb7
Paul Librecht <paul@hoplahup.net>
2011-09-05 18:52:47 +0000

    Rescale now with some more browser tolerance. paul
    svn@36553

--- web/src/main/webapp/skins/curriki8/xwiki.js
+++ web/src/main/webapp/skins/curriki8/xwiki.js
@@ -612,3 +612,13 @@ if ('undefined' !== typeof Ext) {
 		});
 	});
 }
+
+
+// useful for rescaling
+function getDocHeight(D) {
+    return Math.max(
+        Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+        Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+        Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+    );
+}
