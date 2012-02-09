4f6a7575da4dbebdf7be6f9e5c7b9c13b364091b
Paul Libbrecht <paul@hoplahup.net>
2012-02-09 11:56:07 +0100

    complete tool to read all geo data. Probably easily adaptable to other field-sets. paul

--- database/mysql/src/resources/DumpUsersAndCountry.java
+++ database/mysql/src/resources/DumpUsersAndCountry.java
@@ -3,14 +3,20 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.lang.Class;
 import java.lang.String;
+import java.lang.System;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
 
 public class DumpUsersAndCountry {
+    
+    public static String[] fieldNames = {"userName",
+            "first_name","last_name", "country","city"};
+
     public static void main(String[] args) throws Exception {
-        String query = "select stringPropFirst.xws_value as first,\n" +
+        /* String query = "select stringPropFirst.xws_value as first,\n" +
                 "        stringPropLast.xws_value as last,\n" +
                 "        stringPropCountry.xws_value as country,\n" +
                 "        stringPropCity.xws_value as city,\n" +
@@ -33,7 +39,20 @@ public class DumpUsersAndCountry {
                 "              and stringPropLast.xws_name=\"last_name\"\n" +
                 "              and stringPropCountry.xws_name=\"country\"\n" +
                 "              and stringPropCity.xws_name=\"city\"\n" +
-                "  ;";
+                "  ;"; */
+        String query = "select stringProp.xws_value as prop,\n" +
+                "        stringProp.xws_name as propName,\n" +
+                "        userObject.xwo_name as xwikiName\n" +
+                "   from   xwikistrings as stringProp,\n" +
+                "          xwikiobjects as userObject\n" +
+                "   where\n" +
+                "      xwo_classname=\"XWiki.XWikiUsers\" and\n" +
+                "            userObject.xwo_id=stringProp.xws_id\n" +
+                "        and (    stringProp.xws_name =\"first_name\"\n" +
+                "              or stringProp.xws_name =\"last_name\"\n" +
+                "              or stringProp.xws_name =\"country\"\n" +
+                "              or stringProp.xws_name =\"city\")  \n" +
+                "           ORDER BY userObject.xwo_name";
 
         Class.forName("com.mysql.jdbc.Driver");
         Connection conn = DriverManager.getConnection(args[0], args[1], args[2]);
@@ -43,16 +62,39 @@ public class DumpUsersAndCountry {
         Writer out = new OutputStreamWriter(new FileOutputStream(args[3]),"utf-8");
         int n=0;
         out.write("\"first\",\"last\",\"country\",\"city\",\"xwikiName\"\n");
-        while(rs.next()) {
-            for(int i : new int[]{1,2,3,4,5}) {
+        String userName = null, firstName, lastName, country, city;
+        boolean tryMore = true;
+        tryMore = rs.next();
+        HashMap p = new HashMap(5);
+        
+
+        while(tryMore) {
+            userName = rs.getString(3);
+            p.put("userName", userName);
+            while(userName.equals(rs.getString(3))) {
+                //System.out.println(rs.getString(2) + " : " + rs.getString(1));
+                p.put(rs.getString(2), rs.getString(1));
+                tryMore = rs.next();
+                if(!tryMore) break;
+            }
+            //System.out.println("Got map: " + p);
+
+            int i=1;
+            for(String fieldName: fieldNames) {
                 out.write("\"");
-                out.write(rs.getString(i));
+                String value= (String) p.get(fieldName);
+                if(value==null) value = "";
+                out.write(value);
                 out.write("\"");
                 if(i<5) out.write(", ");
+                i++;
             }
+            out.write("\"");
+            if(n%100==0) System.out.println("Outputting " + userName + " (" + n + ").");
+            userName = null;
+            p.clear();
             out.write("\n");
             n++;
-            if(n%100==0) System.out.println("Outputting " + rs.getString(5) + " (" + n + ").");
         }
         out.flush(); out.close();
     }
