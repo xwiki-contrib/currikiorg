39572563f2c928ecf1fdf42bcd44371ee79a3e92
Paul Libbrecht <paul@hoplahup.net>
2013-01-17 22:10:04 +0100

    Now with a static method to send events. paul

--- web/src/main/javascript/curriki/curriki-base.js
+++ web/src/main/javascript/curriki/curriki-base.js
@@ -122,6 +122,32 @@ Curriki.hideLoading = function(multi){
 	}
 }
 
+Curriki.logEvent = function(eventParams, followup) {
+    if(window._gaq) {
+        if(followup) {
+            _gaq.push(eventParams).push(followup);
+        } else {
+            _gaq.push(eventParams);
+        }
+    } else {
+        try{
+            if(followup) {
+                window.top._gaq.push(eventParams).push(followup);
+            } else {
+                window.top._gaq.push(eventParams);
+            }
+            if(console) console.info('Would track: ', page);
+        }catch(e){
+            try{
+                if(console) console.info('Failed to track: ', page);
+            }catch(e){
+
+            }
+        }
+
+    }
+}
+
 Curriki.logView = function(page){
 	// Usage in site example:
 	// <a onClick="javascript:Curriki.logView('/Download/attachment/${space}/${name}/${attach.filename}');"> .. </a>
@@ -132,7 +158,7 @@ Curriki.logView = function(page){
     } else {
 
 		// Double try catch for CURRIKI-5828
-		// This is needed because we can not define if where 
+		// This is needed because we can not define if we
 		// are coming from an embedded search in a resource proxy.
 		// So we need to try to address not the top frame if thats fails.
 		try{
@@ -142,12 +168,12 @@ Curriki.logView = function(page){
                 window.top.pageTrackerQueue = window.top.pageTrackerQueue || new Array();
                 window.top.pageTrackerQueue.push(page);
             }
-			console.info('Would track: ', page);
+			if(console) console.info('Would track: ', page);
 		}catch(e){
 			try{
 	 			window.pageTrackerQueue = window.pageTrackerQueue || new Array();
 		        window.pageTrackerQueue.push(page);
-				console.info('Would track: ', page);
+				if(console) console.info('Would track: ', page);
 			}catch(e){
 
 			}
