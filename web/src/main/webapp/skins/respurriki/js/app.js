06d469c04353b1f019438109165d672ad3996640
jorge <ferco_clpm@hotmail.com>
2013-10-06 10:06:24 -0400

    implemented, the new styles/javascripts for table of contents with dropdown sub items (the sub items are static) CHanged login icons and styles in top header right of the layout

--- web/src/main/webapp/skins/respurriki/js/app.js
+++ web/src/main/webapp/skins/respurriki/js/app.js
@@ -11,4 +11,16 @@ $j(document).ready(function() {
         $j("#dropable-toc").slideToggle();
         return false;
     });
+    
+    $j("body").on("click", "#dropable-toc ul li a.has-dropdown", function() {
+        //lets change the icon arrow
+        var item_status = $j(this).next('ul').css('display');
+        if (item_status === 'none') {
+            $j(this).find('i').removeClass().addClass('icon-caret-down');
+        } else {
+            $j(this).find('i').removeClass().addClass('icon-caret-left');
+        }
+        $j(this).next('ul').slideToggle();
+        return false;
+    });
 });
