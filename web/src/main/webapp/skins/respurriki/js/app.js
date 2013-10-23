d5cfa6e19d17669939dab1204a824aeb9ec1a174
jorge <ferco_clpm@hotmail.com>
2013-10-22 20:43:16 -0400

    enabled basic tooltips, but having troubles with the popovers from titles

--- web/src/main/webapp/skins/respurriki/js/app.js
+++ web/src/main/webapp/skins/respurriki/js/app.js
@@ -23,7 +23,13 @@ jQuery(document).ready(function() {
         jQuery(this).parent().parent().find('ul').slideToggle();
         return false;
     });
-    
+
     //style tables automatically
     jQuery(".asset-display-text table").addClass('table table-bordered table-hover table-striped');
+    //enable tooltips automatically
+    jQuery('.has-tooltip').tooltip({
+        placement: 'top',
+    });
+    
+
 });
