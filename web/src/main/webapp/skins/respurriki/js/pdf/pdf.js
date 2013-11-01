f4adae30334f08377789d4f8e09dc314bf54ec63
jorge <ferco_clpm@hotmail.com>
2013-10-31 20:15:44 -0400

    Pdf embebed basic viewer finished and working, disabled editor js libraries because of jquery conflict, added automatic styles for external links created with curriki8 editor

--- web/src/main/webapp/skins/respurriki/js/pdf/pdf.js
+++ web/src/main/webapp/skins/respurriki/js/pdf/pdf.js
@@ -4433,7 +4433,8 @@ var WorkerTransport = (function WorkerTransportClosure() {
     // Right now, the requirement is, that an Uint8Array is still an Uint8Array
     // as it arrives on the worker. Chrome added this with version 15.
     if (!globalScope.PDFJS.disableWorker && typeof Worker !== 'undefined') {
-      var workerSrc = PDFJS.workerSrc;
+      //var workerSrc = PDFJS.workerSrc;
+      var workerSrc = '/xwiki/skins/respurriki/1111/js/pdf/pdf.worker.js';
       if (!workerSrc) {
         error('No PDFJS.workerSrc specified');
       }
