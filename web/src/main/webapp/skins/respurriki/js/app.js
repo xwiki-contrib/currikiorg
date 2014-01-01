4757d6560c720d84c4e8884d0f959140ecd47f21
Felix <felix@curriki.org>
2014-01-02 00:46:59 +0100

    Work on: CURRIKI-6416 - The Social login using Facebook and Google doesn’t work. Trying to keep backwards compatibility.

--- web/src/main/webapp/skins/respurriki/js/app.js
+++ web/src/main/webapp/skins/respurriki/js/app.js
@@ -1,4 +1,71 @@
-/* 
+// In case we forget to take out console statements. IE becomes very unhappy when we forget. Let's not make IE unhappy
+if (typeof(console) === 'undefined') {
+  var console = {};
+  console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time = console.timeEnd = console.assert = console.profile = function () {
+  };
+}
+
+// Some backwards compatibility to ensure some of the
+// olf functionality keeps flowing.
+if (typeof(Curriki) === 'undefined') {
+  var Curriki = {};
+  Curriki.ui = {};
+  Curriki.ui.login = {};
+}
+
+// Fix the Curriki console
+Curriki.console = window.console;
+
+// Fix for the "BackFromAuthorization"
+
+Curriki.ui.login.finishAuthorizationPopup = function (targetURL, openerWindow, openedWindow, toTop) {
+  Curriki.console.log("Finishing popup, (toTop? " + toTop + ") target: " + targetURL);
+  if (typeof(openerWindow) == "undefined" || openerWindow == window) {
+    openerWindow = window.open(targetURL, "currikiMainWindow");
+  }
+  if (openerWindow) { // && (openerWindow.Curriki.ui.login.authorizeDialog && openerWindow.Curriki.ui.login.authorizeDialog==window || (openerWindow.top.Curriki.ui.login.authorizeDialog && openerWindow.top.Curriki.ui.login.authorizeDialog==window))
+
+    // we are in a popup relationship, can close and revert to that popup
+    Curriki.console.log("We are in popup, closing and opening popup.");
+    var targetWindow = openerWindow;
+    if (openerWindow.Curriki.ui.login.windowThatShouldNextGoTo)
+      targetWindow = openerWindow.Curriki.ui.login.windowThatShouldNextGoTo;
+    Curriki.console.log("targetWindow: " + targetWindow + " with force to top " + toTop);
+    if (toTop) targetWindow = targetWindow.top;
+    //      else if(openerWindow.Ext && openerWindow.Ext.get('loginIframe'))
+    //          targetWindow = openerWindow.Ext.get('loginIframe').dom.contentWindow;
+    if (targetWindow && targetWindow.location) {
+      targetWindow.location.href = targetURL;
+      //alert("Would go to " + targetURL + " from " + targetWindow);
+      // schedule a close
+      openedWindow.setInterval(function () {
+        try {
+          targetWindow.focus();
+        } catch (e) {
+          Curriki.console.log(e);
+        }
+        try {
+          openedWindow.close();
+        } catch (e) {
+          Curriki.console.log(e);
+        }
+      }, 20);
+    } else {
+      var w = window;
+      if (toTop) w = w.top;
+      w.location.href = targetURL;
+    }
+    return false;
+  } else {
+    Curriki.console.log("No popup parent found... ah well.");
+    var w = openedWindow;
+    if (toTop) w = w.top;
+    w.location.href = targetURL;
+    //alert("Would go to " + targetURL + " from " + openedWindow);
+  }
+};
+
+/*
  * Lets automatically convert all images into responsive using jquery and bootstrap
  */
 $.noConflict();
